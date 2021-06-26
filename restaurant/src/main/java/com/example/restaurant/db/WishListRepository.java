b7d2e1da2392f9d0c1bd4ef0292a1b28563b9c6e
Talkikye <83283855+bsh6463@users.noreply.github.com>
2021-06-26 23:41:18 +0900

    Add files via upload

---
+++ restaurant/src/main/java/com/example/restaurant/db/WishListRepository.java
@@ -0,0 +1,10 @@
+package com.example.restaurant.db;
+
+import com.example.restaurant.wishList.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+
+}
